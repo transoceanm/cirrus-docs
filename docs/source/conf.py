49f209f3419a37f56628b8d6283961d8d80cfee8
weijietransm <131146237+weijietransm@users.noreply.github.com>
2024-04-29 12:37:16 +0800

    Add files via upload

---
+++ docs/source/conf.py
@@ -0,0 +1,32 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Cirrus'
+copyright = '2024, Arka'
+author = 'Arka'
+release = '0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+html_theme_options = {
+    'fixed_sidebar': True,
+    'sidebar_width': '250px',
+}
---
+++ docs/source/index.rst
@@ -0,0 +1,18 @@
+==================================
+Welcome to Cirrus's documentation!
+==================================
+
+**Cirrus** is a Modern App Delivery and Hosting Platform for users to manage
+and access resources like Virtual Machines. It offers 
+functionalies such as resource management and usage monitoring 
+to *streamline operations* and *optimize resource usage*.
+
+Contents
+========
+
+.. toctree::
+
+   userinterface
+   networking
+   vpc
+   instances
